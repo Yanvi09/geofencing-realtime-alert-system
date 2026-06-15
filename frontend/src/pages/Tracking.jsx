408c5fb02238a4c58b852aa80834e4409d680429
Yanvi <20yanvi05@gmail.com>
2026-06-15 18:49:06 +0530

    Add live tracking map with leaflet

--- frontend/src/pages/Tracking.jsx
+++ frontend/src/pages/Tracking.jsx
@@ -1,6 +1,25 @@
 import MainLayout from "../layouts/MainLayout";
 
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Polygon,
+  Popup,
+} from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
 function Tracking() {
+  const vehiclePosition = [37.7749, -122.4194];
+
+  const geofence = [
+    [37.7749, -122.4194],
+    [37.7849, -122.4194],
+    [37.7849, -122.4094],
+    [37.7749, -122.4094],
+  ];
+
   return (
     <MainLayout>
       <h1
@@ -14,17 +33,35 @@ function Tracking() {
 
       <div
         style={{
-          height: "500px",
-          background: "#FFF0D9",
-          borderRadius: "16px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "24px",
-          fontWeight: "600",
+          height: "600px",
+          borderRadius: "20px",
+          overflow: "hidden",
         }}
       >
-        Map Coming Next
+        <MapContainer
+          center={vehiclePosition}
+          zoom={14}
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <Marker position={vehiclePosition}>
+            <Popup>
+              Vehicle KA-01-AB-1234
+            </Popup>
+          </Marker>
+
+          <Polygon positions={geofence}>
+            <Popup>
+              Downtown Zone
+            </Popup>
+          </Polygon>
+        </MapContainer>
       </div>
     </MainLayout>
   );
